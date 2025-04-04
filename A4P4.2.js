28c8c5886ac4c9d120e5698c8d3e18a42b17abb4
chriscapp5766 <christopher.cappuccitti@dcmail.ca>
2025-04-03 23:25:05 -0400

    section 2 of part 4 commit 4
    in this section it is partially the same as part 1 what this does initially is it updates the size of the balls bouncing around on the screen. and also when the ball hits one of the walls it will change color and size. the new part that was added to this is the evil circle which will start at one point on the screen which is inputted into the code and its color is set to white  what else I did is also set key binds so that you are able to control the evil circle

---
+++ A4P4.2.js
@@ -0,0 +1,60 @@
+update() {
+    if (this.x + this.size >= width) {
+      this.velX = -Math.abs(this.velX);
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = Math.abs(this.velX);
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -Math.abs(this.velY);
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = Math.abs(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (!(this === ball) && ball.exist) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}
+
+class EvilCircle extends shape {
+  constructor(x,y) {
+    super(x,y,20,20)
+    this.color="white"
+    this.size = 10;
+
+    // inputs for keyboard  to move object
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+  });
+  }
