4a738601004d343b68c3b42971e899e6e3906b3e
chriscapp5766 <christopher.cappuccitti@dcmail.ca>
2025-04-03 23:13:17 -0400

    part 3 of javascript part 3 commit 5
    with this final bit of code I used the collision function this was used to when the ball hits the edge of the screen it will then change color and continue bouncing across the screen. and while this is happening I made a while statement that says while the ball is smaller then 25 it will get resized backed to 25 and pulled away from any corners on the screen

---
+++ A4P3.3.js
@@ -0,0 +1,47 @@
+collisionDetect() {
+    for (const ball of balls) {
+      if (!(this === ball)) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}
+
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always at one ball width
+    //moves ball away from edge of the screen
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
